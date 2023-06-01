11b99beec58b013c899d2d21fa7c1adae3690063
MaishaFN <jose.maisha@gmail.com>
2023-06-01 15:07:34 +0200

    Admin routes done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -10,6 +10,7 @@ router.use("/group", require("./group.routes"));
 router.use("/publication", require("./publication.routes"));
 router.use("/valuation", require("./valuation.routes"));
 router.use("/games", require("./games.routes"));
+router.use("/admin", require("./admin.routes"));
 
 
 
