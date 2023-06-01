c104ac8604d08a4d66c756abb7177d01d701bad8
Jesús Ruiz <jesusruizjaen@gmail.com>
2023-06-01 15:25:37 +0200

    testing the api

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -5,8 +5,19 @@ const axios = require("axios");
 router.get("/list", async (req, res, next) =>{
     try {
         const response = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}`);
-        console.log(response.data.results.length)
-        res.json(response.data.result);
+        const response2 = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}&page=2`);
+        const response3 = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}&page=2`);
+        const response4 = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}&page=2`);
+        const response5 = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}&page=2`);
+
+        const list1 = response.data.results;
+        const list2 = response2.data.results;
+        const list3 = response3.data.results;
+        const list4 = response4.data.results;
+        const list5 = response5.data.results;
+
+        console.log(response.data.results)
+        res.json([...list1, ...list2, ...list3, ...list4, ...list5]);
     } catch (error) {
         next(error)
     }
@@ -16,7 +27,7 @@ router.get("/list", async (req, res, next) =>{
 router.get("/:gameId/details", async (req, res, next) =>{
     try {
         const response = await axios.get(`https://api.rawg.io/api/games/${req.params.gameId}?key=${process.env.RAWG_KEY}`);
-        res.json(response.data);
+        res.json(response.data.results);
     } catch (error) {
         next(error)
     }
@@ -27,7 +38,7 @@ router.get("/:gameId/details", async (req, res, next) =>{
 router.get("/list/:queryValue", async (req, res, next) =>{
     try {
         const response = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}`);
-        const queryGames = response.data.filter(game => game.title.includes(req.params.queryValue));
+        const queryGames = response.data.results.filter(game => game.name.includes(req.params.queryValue));
         res.json(queryGames);
     } catch (error) {
         next(error)
