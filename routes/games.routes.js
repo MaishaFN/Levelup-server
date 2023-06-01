87cbfad9604bd7464a4ca3f57fdc5d6d2389ec4e
Jesús Ruiz <jesusruizjaen@gmail.com>
2023-06-01 15:54:58 +0200

    some changes in games routes

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -1,32 +1,23 @@
 const router = require("express").Router();
 const axios = require("axios");
 
-//GET "/list" => All game list
-router.get("/list", async (req, res, next) =>{
+
+//GET "/list/:page" => All game list next page
+router.get("/list/:page", async (req, res, next) =>{
     try {
-        const response = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}`);
-        const response2 = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}&page=2`);
-        const response3 = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}&page=2`);
-        const response4 = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}&page=2`);
-        const response5 = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}&page=2`);
+        const response = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}&page=${req.params.page}`);
 
-        const list1 = response.data.results;
-        const list2 = response2.data.results;
-        const list3 = response3.data.results;
-        const list4 = response4.data.results;
-        const list5 = response5.data.results;
-
-        console.log(response.data.results)
-        res.json([...list1, ...list2, ...list3, ...list4, ...list5]);
+        res.json([...response.data.results]);
     } catch (error) {
         next(error)
     }
 })
 
+
 //GET "/:gameId/details" => Game details
 router.get("/:gameId/details", async (req, res, next) =>{
     try {
-        const response = await axios.get(`https://api.rawg.io/api/games/${req.params.gameId}?key=${process.env.RAWG_KEY}`);
+        const response = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}/${req.params.gameId}`);
         res.json(response.data.results);
     } catch (error) {
         next(error)
@@ -37,9 +28,9 @@ router.get("/:gameId/details", async (req, res, next) =>{
 
 router.get("/list/:queryValue", async (req, res, next) =>{
     try {
-        const response = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}`);
-        const queryGames = response.data.results.filter(game => game.name.includes(req.params.queryValue));
-        res.json(queryGames);
+        const game = req.params.queryValue.split(" ").join("-");
+        const response = await axios.get(`https://api.rawg.io/api/games?key=${process.env.RAWG_KEY}&search=${game}`);
+        res.json(response.data.results);
     } catch (error) {
         next(error)
     }
